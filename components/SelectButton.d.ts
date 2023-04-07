be99f1d9f3ed643409205977b58446273e58d580
Lukas Nielsen <moin@lukasnielsen.de>
2023-04-07 13:21:37 +0200

    add SelectButton

---
+++ components/SelectButton.d.ts
@@ -0,0 +1,39 @@
+import { CSSProperties } from "react";
+
+export function SelectButton({
+	onSelect,
+	title = "",
+	description = "",
+	disabled = false,
+	label = "Select",
+	list,
+	listKey = "name",
+	listValue = "value",
+	selectedFlag = "isSelected",
+	multiSelect = false,
+	quickFind = false,
+	selectAllButton,
+	className,
+	style,
+	showSelection = true,
+	showListSelection = true,
+	stopPropagation = false,
+}: {
+	onSelect?: (e: { buttonType: number; selection: object[] }) => void;
+	title: string;
+	description: string;
+	disabled: boolean;
+	label: string;
+	list: object[];
+	listKey: string;
+	listValue: string;
+	selectedFlag: string;
+	multiSelect: boolean;
+	quickFind: boolean;
+	selectAllButton?: string;
+	className?: string;
+	style?: CSSProperties;
+	showSelection: boolean | number;
+	showListSelection: boolean;
+	stopPropagation: boolean;
+}): JSX.Element;
